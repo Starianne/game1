3c9b7233f32e6685b6a78441780c6c3e118c7510
Annelies <118388665+Starianne@users.noreply.github.com>
2025-12-11 20:36:00 +0000

    added test text (I am not keeping this)

--- venv/main.py
+++ venv/main.py
@@ -5,9 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('My game', False, 'Green')
 
 while True:
     for event in pygame.event.get():
@@ -18,6 +20,7 @@ while True:
         
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface,(300, 50))
     
     # draw all of our elements + update everything
     pygame.display.update()
