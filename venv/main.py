a2d3fbd2318a4a8f62eaa0ea01da96ae0d3a201d
Annelies <118388665+Starianne@users.noreply.github.com>
2025-12-12 17:21:11 +0000

    Centering text and looked at collisions

--- venv/main.py
+++ venv/main.py
@@ -5,11 +5,14 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+text_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render('My game', False, 'Black').convert()
+
+title_surface = text_font.render('My game', False, 'Black').convert()
+title_rect = title_surface.get_rect(center =(400,100))
 
 snail_surface= pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(bottomright = (600, 300))
@@ -23,27 +26,33 @@ while True:
             pygame.quit()
             exit()
 
-        if event.type ==pygame.MOUSEBUTTONUP:
-            print('mouse up')
+        if event.type ==pygame.MOUSEMOTION:
+            #print(event.pos)
+            if player_rect.collidepoint(event.pos):
+                print("boo")
 
         
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface,(300, 50))
+    pygame.draw.rect(screen, 'Pink', title_rect)
+    pygame.draw.rect(screen, 'Pink', title_rect,10)
+    screen.blit(title_surface,title_rect)
+
     snail_rect.x -= 4
     if snail_rect.right <= 0:
         snail_rect.left = 800
     screen.blit(snail_surface,snail_rect)
+
     player_rect.left += 1
     screen.blit(player_surf,player_rect)
     
-    if player_rect.colliderect(snail_rect):
-        print('collision')
+    #if player_rect.colliderect(snail_rect):
+        #print('collision')
 
 
-    mouse_pos = pygame.mouse.get_pos()
-    if player_rect.collidepoint(mouse_pos):
-        print(pygame.mouse.get_pressed())
+    #mouse_pos = pygame.mouse.get_pos()
+    #if player_rect.collidepoint(mouse_pos):
+        #print(pygame.mouse.get_pressed())
 
     # draw all of our elements + update everything
     pygame.display.update()
