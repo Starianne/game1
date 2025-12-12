405251eb2898f6bb112fe1263277854d858a197d
Annelies <118388665+Starianne@users.noreply.github.com>
2025-12-12 21:19:19 +0000

    Making game inactive page nicer, next I'll add a score

--- venv/main.py
+++ venv/main.py
@@ -27,9 +27,7 @@ text_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
-#title
-title_surface = text_font.render('My game', False, (64,64,64)).convert()
-title_rect = title_surface.get_rect(center =(400,100))
+
 
 #snail
 snail_surface= pygame.image.load('graphics/snail/snail1.png').convert_alpha()
@@ -39,6 +37,17 @@ snail_rect = snail_surface.get_rect(bottomright = (600, 300))
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300))
 player_gravity = 0
+
+#intro
+player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+player_stand_rect = player_stand.get_rect(center=(400,200))
+#title - intro
+title_surface = text_font.render('My game', False, (111,196,169)).convert()
+title_rect = title_surface.get_rect(center =(400,50))
+
+game_message = text_font.render("Press space to run",False,(111,196,169))
+game_message_rect = game_message.get_rect(center = (400,320))
 
 while True:
     for event in pygame.event.get():
@@ -54,7 +63,7 @@ while True:
                 if event.key == pygame.K_SPACE and player_rect.bottom == 300: #space = jump only if on floor
                     player_gravity = -20
         else:
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
                 start_time = pygame.time.get_ticks()
@@ -64,11 +73,7 @@ while True:
         screen.blit(sky_surface, (0,0))
         screen.blit(ground_surface, (0,300))
 
-        #title surface
-        pygame.draw.rect(screen, '#c0e8ec', title_rect)
-        pygame.draw.rect(screen, '#c0e8ec', title_rect,20)
-
-        screen.blit(title_surface,title_rect)
+        
 
         #snail movement
         snail_rect.x -= 4
@@ -92,10 +97,12 @@ while True:
         screen.blit(player_surf,player_rect)
         display_score()
     else:
-        screen.fill('Yellow')
-        if event.type == pygame.KEYDOWN:
-            game_active = True
+        screen.fill((94,129,162))
+        screen.blit(player_stand, player_stand_rect)
+        screen.blit(title_surface,title_rect)
+        screen.blit(game_message, game_message_rect)
 
+        
     # draw all of our elements + update everything
     pygame.display.update()
     clock.tick(60) #this while true loop should not run faster than 60 frames per second
