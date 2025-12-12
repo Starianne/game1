37fe9a8f2f65c48193c39d500130d08b51e5a2fe
Annelies <118388665+Starianne@users.noreply.github.com>
2025-12-12 11:12:00 +0000

    I love my snail

--- venv/main.py
+++ venv/main.py
@@ -5,11 +5,14 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font(None, 50)
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
-text_surface = test_font.render('My game', False, 'Green')
+text_surface = test_font.render('My game', False, 'Black')
+
+snail_surface= pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos = 600
 
 while True:
     for event in pygame.event.get():
@@ -21,6 +24,10 @@ while True:
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
     screen.blit(text_surface,(300, 50))
+    snail_x_pos -= 4
+    if snail_x_pos < -100:
+        snail_x_pos = 800
+    screen.blit(snail_surface,(snail_x_pos,250))
     
     # draw all of our elements + update everything
     pygame.display.update()
