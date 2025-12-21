27b6f136c40d4dab5b808d5a00b3b7e9e490f628
[Annelies S] <118388665+Starianne@users.noreply.github.com>
2025-12-21 21:48:03 +0000

    Finished player sprite class

--- main.py
+++ main.py
@@ -3,15 +3,45 @@ from sys import exit
 from random import randint 
 
 class Player(pygame.sprite.Sprite): #inherits from pygame class
+    
     def __init__(self):
         super().__init__() #overriding class
-        self.image = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+        player_walk_1 = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+        player_walk_2 = pygame.image.load('graphics/player/player_walk_2.png').convert_alpha()
+        self.player_walk = [player_walk_1,player_walk_2]
+        self.player_index = 0
+        self.player_jump = pygame.image.load('graphics/player/jump.png').convert_alpha()
+
+
+        self.image = self.player_walk[self.player_index]
         self.rect = self.image.get_rect(midbottom =(200,300))
+        self.gravity = 0
 
     def player_input(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
-            print("hi")
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 300: #makes player jump
+            self.gravity = -20
+
+    def apply_gravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 300:
+            self.rect.bottom = 300
+
+    def animation_state(self):
+        if self.rect.bottom < 300:
+            self.image = self.player_jump
+        else:
+            self.player_index += 0.1
+            if self.player_index >= len(self.player_walk):
+                self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
+        self.animation_state()
+
 
 
 
@@ -195,6 +225,7 @@ while True:
         #player surface
         screen.blit(player_surf,player_rect)
         player.draw(screen)
+        player.update()
 
         display_score()
     else:
